c94fddd1be57cac4f3444ddc6c01d10b413ae0c4
victorwu2011 <victorwu2011@gmail.com>
2017-01-07 13:19:41 -0500

    springbootEDI

---
+++ SpringBootEDI/src/main/java/EDI/Spring/Boot/SpringBootEdiApplication.java
@@ -0,0 +1,12 @@
+package EDI.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEdiApplication.class, args);
+	}
+}
---
+++ SpringBootEDI/src/test/java/EDI/Spring/Boot/SpringBootEdiApplicationTests.java
@@ -0,0 +1,16 @@
+package EDI.Spring.Boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootEdiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
